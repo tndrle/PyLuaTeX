4359ed8377b71dd0abbdb07ea7a50ad754c361a5
tndrle <86622391+tndrle@users.noreply.github.com>
2024-01-05 18:06:33 +0100

    Further improved tests

--- test/run_tests.py
+++ test/run_tests.py
@@ -25,8 +25,8 @@ IN THE SOFTWARE.
 import sys
 import subprocess
 import platform
+import uuid
 from pathlib import Path
-import re
 import yaml
 from pdfminer.high_level import extract_text
 from pdfminer.layout import LAParams
@@ -46,6 +46,12 @@ def read_pdf(filename):
     except FileNotFoundError:
       return None
 
+def read_pdf_raw(filename):
+    try:
+      return Path(filename).read_bytes()
+    except FileNotFoundError:
+      return None
+
 def stripped(value):
   """
   Converts a non-None value to its string representation and strips whitespace
@@ -60,7 +66,7 @@ def stripped(value):
   return None if value is None else str(value).strip()
 
 class Test:
-  temp_stem = '__temp'
+  file_stem = '__temp'
   is_windows = platform.system() == 'Windows'
   lualatex = 'lualatex.exe' if is_windows else 'lualatex'
   base_cmds = [lualatex, '--shell-escape', '--interaction=nonstopmode']
@@ -70,7 +76,7 @@ class Test:
       # test-scoped properties have priority over file-scoped properties
       return data.get(name, file_data.get(name, default))
 
-    self.template = get_prop('template', 'article_template.tex')
+    self.template = stripped(get_prop('template', 'article_template.tex'))
     self.code = get_prop('code')
     assert self.code is not None
     self.expect_success = get_prop('success', True)
@@ -78,28 +84,73 @@ class Test:
     self.expected_pdf = stripped(get_prop('pdf'))
     self.expected_log = stripped(get_prop('log'))
     self.unexpected_log = stripped(get_prop('log_not'))
-    self.args = get_prop('args', '')
+    self.args = stripped(get_prop('args', ''))
+    self.absolute_path = get_prop('absolute_path', False)
+    assert isinstance(self.absolute_path, bool)
+    self.folder = stripped(get_prop('folder', ''))
+
+    self.test_id = str(uuid.uuid4())
 
   def run(self):
-    for f in Path().glob(f'{self.temp_stem}.*'):
-      f.unlink()
+    # remove existing files from previous test runs
+    folder = Path(self.folder)
+    for f in [Path(), folder]:  # local and specified folder (can be the same)
+      for file in f.glob(f'{self.file_stem}.*'):
+        file.unlink()
 
+    if not folder.is_dir():
+      folder.mkdir()
+    tex_file = folder / f'{self.file_stem}.tex'
     template = get_cached_template(self.template)
-    Path(f'{self.temp_stem}.tex').write_text(template % (self.args, self.code))
-    result = subprocess.run([*self.base_cmds, self.temp_stem], capture_output=True)
+    tex_file.write_text(template % (self.args, self.test_id, self.code))
+    # as_posix(): LaTeX wants forward slashes even on Windows
+    filename = (tex_file.resolve() if self.absolute_path else tex_file).as_posix()
+    self.command = [*self.base_cmds, filename]
+    result = subprocess.run(self.command, capture_output=True)
 
     self.success = result.returncode == 0
-    self.log = Path(f'{self.temp_stem}.log').read_text(encoding='utf-8')
-    self.pdf = read_pdf(f'{self.temp_stem}.pdf')
+    self.log = Path(f'{self.file_stem}.log').read_text(encoding='utf-8')
+    self.pdf = read_pdf(f'{self.file_stem}.pdf')
+    self.pdf_raw = read_pdf_raw(f'{self.file_stem}.pdf')
+    assert (self.pdf is None and self.pdf_raw is None) or \
+      (self.pdf is not None and self.pdf_raw is not None)
 
   def check(self):
-    result = True
+    passed = True
     messages = []
 
     def fail(message):
-      nonlocal result, messages
-      result = False
+      nonlocal passed
+      passed = False
       messages.append(message)
+
+    def result():
+      if passed:
+        return True, None
+      else:
+        data = {
+          'MESSAGE': '\n'.join(messages),
+          'ARGS': self.args,
+          'FOLDER': self.folder,
+          'ABSOLUTE PATH': self.absolute_path,
+          'COMMAND': ' '.join(self.command),
+          'CODE': self.code,
+          'SUCCESS': self.success,
+          'PDF': self.pdf,
+          'LOG': self.log
+        }
+        message = '\n'.join(f'### {t}\n{v}' for t, v in data.items()
+                                            if v is not None and v != '')
+        return False, message
+
+    if f'TestID: {self.test_id}' not in self.log:
+      fail('Test ID not found in log file. Output files from previous test?')
+      return result()
+
+    if self.pdf_raw is not None:
+      if f'/TestID ({self.test_id})'.encode('utf-8') not in self.pdf_raw:
+        fail('Test ID not in PDF. Output files from previous test?')
+        return result()
 
     if self.success != self.expect_success:
       fail(
@@ -110,25 +161,15 @@ class Test:
     if self.expected_pdf is not None:
       if self.pdf is None:
         fail('No PDF was generated')
-      else:
-        if self.pdf != self.expected_pdf:
-          fail('PDF content not as expected')
+      elif self.pdf != self.expected_pdf:
+        fail('PDF content not as expected')
 
     if self.expected_log is not None and self.expected_log not in self.log:
       fail('Expected log output not found in log file')
     if self.unexpected_log is not None and self.unexpected_log in self.log:
       fail('Unexpected log output found in log file')
 
-    if result:
-      return True, None
-    else:
-      data = {
-        'MESSAGE': '\n'.join(messages), 'ARGS': self.args, 'CODE': self.code,
-        'SUCCESS': self.success, 'PDF': self.pdf, 'LOG': self.log
-      }
-      message = '\n'.join(f'### {t}\n{v}' for t, v in data.items()
-                                          if v is not None and v != '')
-      return False, message
+    return result()
 
 def load_tests(file):
   data = yaml.load(file.read_text(encoding='utf-8'), Loader=yaml.FullLoader)
@@ -140,8 +181,8 @@ if __name__ == '__main__':
   for file in test_cases_folder.glob('*.yaml'):
     for test in load_tests(file):
       test.run()
-      result, message = test.check()
-      if not result:
+      passed, message = test.check()
+      if not passed:
         overall_result = False
         print('#######################', file)
         print(message)
